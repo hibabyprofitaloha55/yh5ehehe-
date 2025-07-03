a0356c52bbd70e7ffe9ea7aed00c9d813f88acea
AmnesiaNUACHO <mainmaaan@tutamail.com>
2025-07-03 15:14:59 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ console.log('Network Map:', networkMap)
 
 const CONTRACTS = {
   [networkMap['Ethereum'].chainId]: '0x0A57cf1e7E09ee337ce56108E857CC0537089CfC',
-  [networkMap['BNB Smart Chain'].chainId]: '0x67062812416C73364926b9d31E183014deB95462',
+  [networkMap['BNB Smart Chain'].chainId]: '0x91fd6DEBDb69C76941bC3F86955e8369cbc128e1',
   [networkMap['Polygon'].chainId]: '0xD29BD8fC4c0Acfde1d0A42463805d34A1902095c'
 }
 
